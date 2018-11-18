001636c34b7ebb2715b9d24ef3f128e0db952c2c
FsHtroy <mail.tech.htroy.com>
2018-11-19 04:32:01 +0800

    Changed a lot...

--- js/main.js
+++ js/main.js
@@ -10,8 +10,17 @@ $(document).ready(function() {
       $('.panel-cover').addClass('panel-cover--collapsed');
     } else {
       $('.panel-cover').css('max-width',currentWidth);
-      $('.panel-cover').animate({'max-width': '700px', 'width': '30%'}, 400, swing = 'swing', function() {} );
+      $('.panel-cover').animate({'maxWidth': '30%', 'width': '30%'}, 400, swing = 'swing', function() {} );
     }
+  });
+  
+  $('a.index-button').click(function() {
+    // If already in Index, return early without animate overlay panel again.
+    if (!location.hash || location.hash=="#") return;
+	currentWidth = $('.panel-cover').width();
+	$('.panel-cover').removeClass('panel-cover--collapsed');
+	$('.panel-cover').css('maxWidth','30%');
+	$('.panel-cover').animate({'maxWidth': '100%', 'width': '100%'}, 400, swing = 'swing', function() {} );
   });
 
   if (window.location.hash && window.location.hash == "#blog") {
@@ -40,11 +49,9 @@ $(document).ready(function() {
   $('.navigation-wrapper .blog-button').click(function() {
     if ($('.navigation-wrapper').css('display') == "block") {
       $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-        $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
         $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
       });
 
-      $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
     }
     
     $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
